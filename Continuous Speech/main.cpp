bd3d69efc9a636dcd72014ee0813c6b4085e206b
Htiango <hongty106@gmail.com>
2016-11-03 14:55:05 +0800

    Initial Commit

---
+++ Continuous Speech/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Continuous Speech
+//
+//  Created by hty on 03/11/2016.
+//  Copyright © 2016 hty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
